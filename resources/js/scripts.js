aada6176f6bc67ab69b9ccbbc195c8037c8a369d
meeks21 <anthony.meeks3@gmail.com>
2020-10-31 16:33:48 -0700

    adds jquery and finishes media queries

---
+++ resources/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon ion-icon')
+
+        nav.slideToggle(200);
+
+        <ion-icon name="close"></ion-icon>
+
+    });
+   
+
+});
