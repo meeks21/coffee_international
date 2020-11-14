c6b1dc33d04203cdfc580cff23b7472a2b4de51b
meeks21 <anthony.meeks3@gmail.com>
2020-11-13 18:10:16 -0800

    upadates sticky navigation

--- resources/js/events.js
+++ resources/js/events.js
@@ -1,7 +1,36 @@
-let submitButton = document.querySelector(".btn-ghost");
 
-const submitFunction = () => {
-    submitButton.style.color = "blue"
+/****************************************************************************
+*
+* The following script hides the top nav bar once the paged is scrolled
+* 
+***************************************************************************/
+
+const hideTopNavBar = () => {
+
+    const scrolled = window.scrollY;
+    // assigns the window object scrollY property to the variable "scrolled"
+
+    if (scrolled >= 33) {
+               
+        let sticky = document.querySelector('.js--sticky')
+        
+        sticky.classList.add('sticky');
+
+        document.querySelector('.sticky').style.cssText = "top: 0;";
+
+    } else if (scrolled < 33) {
+
+        let sticky = document.querySelector('.js--sticky')
+
+        document.querySelector('.sticky').style.cssText = "top: 26.78px; transition: top 0.6s;";
+
+        if (scrolled <= 22 ) {
+
+            sticky.classList.remove('sticky');
+
+        }        
+    }            
 }
 
-submitButton.addEventListener('click', submitFunction)
+
+window.addEventListener('scroll', hideTopNavBar);
